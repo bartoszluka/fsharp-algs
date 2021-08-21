1237882f8ad63d9e0f5592ff72babad897a65b1c
bartoszluka <bartoszluka1@gmail.com>
2021-08-21 14:10:29 +0200

    Add my own partition function

--- Algs/Sorts.fs
+++ Algs/Sorts.fs
@@ -21,12 +21,55 @@ let rec mergeSort (list: int list) =
         let listB = snd lists
         merge (mergeSort listA) (mergeSort listB)
 
+let partition predicate (list: int list) =
+    let rec partHelper pred lst pair =
+        match lst with
+        | [] -> pair
+        | x :: xs ->
+            let (left, right) = pair
+
+            if pred x then
+                partHelper pred xs (x :: left, right)
+            else
+                partHelper pred xs (left, x :: right)
+
+    partHelper predicate list ([], [])
+
 let rec quickSort (list: int list) =
     match list with
     | [] -> []
     | x :: xs ->
-        let smaller = List.filter ((>) x) xs
-        let bigger = List.filter ((<=) x) xs
+        let (smaller, bigger) = partition ((>) x) xs
 
-        // quickSort smaller @ [ x ] @ quickSort bigger
         quickSort smaller @ (x :: quickSort bigger)
+
+
+// let bubbleSortMutable (l: Foo list) =
+//     let mutable l' = l |> Array.ofList
+//     let mutable keepGoing = true
+
+//     while keepGoing do
+//         keepGoing <- false
+
+//         for i in [ 1 .. (Array.length l' - 1) ] do
+//             if l'.[i - 1] > l'.[i] then
+//                 let t = l'.[i - 1]
+//                 l'.[i - 1] <- l'.[i]
+//                 l'.[i] <- t
+//                 keepGoing <- true
+
+//     l' |> List.ofArray
+
+// let bubbleSortRecursive (l: Foo list) =
+//     let rec bubbleSort' a rev l =
+//         match l, rev with
+//         | [], true -> List.rev a
+//         | [], false -> List.rev a |> bubbleSort' [] true
+//         | h1 :: h2 :: t, _ ->
+//             if h1 > h2 then
+//                 bubbleSort' (h2 :: a) false (h1 :: t)
+//             else
+//                 bubbleSort' (h1 :: a) rev (h2 :: t)
+//         | h :: t, _ -> bubbleSort' (h :: a) rev t
+
+//     bubbleSort' [] true l
