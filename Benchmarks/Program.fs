d31304d5e27b4cc7cc8b06272d3411ea92ed7fce
bartoszluka <bartoszluka1@gmail.com>
2021-08-21 14:08:35 +0200

    Extend benchmarks for future algorithms

--- Benchmarks/Program.fs
+++ Benchmarks/Program.fs
@@ -5,54 +5,30 @@ open Sorts
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
 
-let listSort = List.sort
 
-// let bubbleSortMutable (l: Foo list) =
-//     let mutable l' = l |> Array.ofList
-//     let mutable keepGoing = true
-
-//     while keepGoing do
-//         keepGoing <- false
-
-//         for i in [ 1 .. (Array.length l' - 1) ] do
-//             if l'.[i - 1] > l'.[i] then
-//                 let t = l'.[i - 1]
-//                 l'.[i - 1] <- l'.[i]
-//                 l'.[i] <- t
-//                 keepGoing <- true
-
-//     l' |> List.ofArray
-
-// let bubbleSortRecursive (l: Foo list) =
-//     let rec bubbleSort' a rev l =
-//         match l, rev with
-//         | [], true -> List.rev a
-//         | [], false -> List.rev a |> bubbleSort' [] true
-//         | h1 :: h2 :: t, _ ->
-//             if h1 > h2 then
-//                 bubbleSort' (h2 :: a) false (h1 :: t)
-//             else
-//                 bubbleSort' (h1 :: a) rev (h2 :: t)
-//         | h :: t, _ -> bubbleSort' (h :: a) rev t
-
-//     bubbleSort' [] true l
-
-let initList n =
+let initRandomList n =
     let rand = Random()
 
-    [ 1 .. n ]
-    |> List.map (fun _ -> rand.Next(10 * n))
+    List.init n (fun _ -> rand.Next(10 * n))
 
+let initSortedList n = initRandomList n |> List.sort
+
+let initReversedList n = initRandomList n |> List.sortDescending
+
+
+[<AbstractClass>]
 [<MemoryDiagnoser>]
 type SortComparison() =
 
-    [<Params(10, 100, 1000)>]
+    [<Params(10, 100, 10000)>]
     member val ListSize: int = 0 with get, set
 
-    member self.MainList = initList self.ListSize
+    abstract member Initializer : (int -> int list)
+
+    member self.MainList = self.Initializer self.ListSize
 
     [<Benchmark(Baseline = true)>]
-    member self.ListSort() = listSort self.MainList
+    member self.ListSort() = List.sort self.MainList
 
     [<Benchmark>]
     member self.ListQuickSort() = quickSort self.MainList
@@ -62,9 +38,32 @@ type SortComparison() =
 
 
 
+type SortComparisonRandom() =
+    inherit SortComparison()
+
+    override _.Initializer = initRandomList
+
+
+type SortComparisonReversed() =
+    inherit SortComparison()
+
+    override _.Initializer = initReversedList
+
+type SortComparisonSorted() =
+    inherit SortComparison()
+
+    override _.Initializer = initSortedList
+
+
 [<EntryPoint>]
 let main _ =
-    BenchmarkRunner.Run typeof<SortComparison>
+    BenchmarkRunner.Run typeof<SortComparisonRandom>
+    |> ignore
+
+    BenchmarkRunner.Run typeof<SortComparisonSorted>
+    |> ignore
+
+    BenchmarkRunner.Run typeof<SortComparisonReversed>
     |> ignore
 
     0
